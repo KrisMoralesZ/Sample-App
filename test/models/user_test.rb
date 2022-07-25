76f23f20a9c0e756421699c99dd242917676f65b
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-25 10:35:15 -0700

    test for uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase\
-  
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
@@ -25,4 +25,26 @@ class UserTest < ActiveSupport::TestCase\
     assert_not @user.valid?
   end
 
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email shoul reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_addresses|
+      @user.email = invalid_addresses
+      assert_not @user.valid?, "#{invalid_addresses.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
