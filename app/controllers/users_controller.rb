1e3f91cf8a96910fbf1440021674213c96ee71bb
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-31 17:36:59 -0700

    user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   private
-  def user_parmas
-    parmans.require(:user).permit(:name, :email, :password, :password_confiormation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
