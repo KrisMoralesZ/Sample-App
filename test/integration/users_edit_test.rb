8071ecffbae442b40338ac0b938607436cae0f2e
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-08-22 12:22:50 -0700

    test require login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsucceful edit" do
+    log_in_as (@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {
@@ -21,6 +22,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "succesful edit" do
+    log_in_as (@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "user"
