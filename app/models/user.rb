8e5d7c3a1ea62fb48da9b0f06bf4beb065dadccb
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-04 07:51:36 -0700

    Add Account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
@@ -38,6 +39,14 @@ class User < ApplicationRecord
 
   def self.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def activate
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
