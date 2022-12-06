f574dfce179e88747e084510d714f0bd069ccddc
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-06 15:19:44 -0700

    a little test

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
+  resources :account_activation, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 end
