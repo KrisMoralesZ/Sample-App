78563efc194447a210923bbe79a5157e6ee4b14a
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-14 17:42:42 -0700

    integration test

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -8,7 +8,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
-      delete micropost_path(@micropost)
+      post micropost_path, params: {micropost: { content: "Lorem ipsum" } }
     end
     assert_redirected_to login_url
   end
@@ -18,5 +18,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:user))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+
+    #Invalid submision
+    assert_no_difference 'Micropost.count' do
+      post micropost_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'a[href=?]', '/?page=2'
+
+    #Valid submision
+    content = "This micropost really ties the room together"
+    assert_diference 'Micropost.count', 1 do
+      post micropost_path, params: {micropost: { content: content } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    
+    #Delete Post
+    assert_select 'a' text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+
+    #Visit different user (no delete links)
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+  
+end
