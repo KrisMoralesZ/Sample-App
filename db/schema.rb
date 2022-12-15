326ec32704ee3c1bc1e3f8a2d047bc4ec9eef976
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-15 16:58:43 -0700

    relationships tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_15_211650) do
+ActiveRecord::Schema.define(version: 2022_12_15_235134) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2022_12_15_211650) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "\"folowed_id\"", name: "index_relationships_on_folowed_id"
+    t.index "\"folower_id\"", name: "index_relationships_on_folower_id"
+    t.index "\"folower_id\", \"folowed_id\"", name: "index_relationships_on_folower_id_and_folowed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
