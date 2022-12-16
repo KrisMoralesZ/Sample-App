ed82e20cf3dbfc78769583d7b683c49156f07458
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-16 10:32:26 -0700

    relationship test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -32,10 +32,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-
-    #Visit different user (no delete links)
-    get user_path(users(:archer))
-    assert_select 'a', text: 'delete', count: 0
+    
   end
   
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationsip = Relationship.new(follower_id: users(:user).id, folowed_id: users(:other_user).id)
+  end
+
+  test "sould be valid" do
+    assert @relationsip.valid?
+  end
+
+  test "should require a follower_id" do
+    @relationsip.follower_id = nil
+    assert_not @relationsip.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationsip.followed_id = nil
+    assert @relationsip.valid?
+  end
 end
