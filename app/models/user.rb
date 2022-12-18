1b2f6320cca77a012467d6acfd45c7b0cf5543c2
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-18 14:26:31 -0700

    folowing test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :pasive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :pasive_relationships, source: :follower
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
