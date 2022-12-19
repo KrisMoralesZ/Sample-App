cd70a010286cdb9351b283e415a6207e0bd07e74
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-19 16:09:33 -0700

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,8 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
-  #has_many :following, :through => :active_relationships,  source: 'followed_id'
-  #has_many :followers, :through => :passive_relationships, source: 'follower_id'
-  has_many :following, :through => :active_relationships, :source => :follower
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
