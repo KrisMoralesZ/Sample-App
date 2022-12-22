73b8707361126a30011e482660ca566e8e54df1c
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-22 10:12:06 -0700

    fix typo on test

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
   def setup
-    @relationship = Relationship.new(follower_id: users(:user).id, folowed_id: users(:other_user).id)
+    @relationship = Relationship.new(follower_id: users(:user).id, followed_id: users(:other_user).id)
   end
 
   test "sould be valid" do
-    assert @relationsip.valid?
+    assert @relationship.valid?
   end
 
   test "should require a follower_id" do
     @relationsip.follower_id = nil
-    assert_not @relationsip.valid?
+    assert_not @relationship.valid?
   end
 
   test "should require a followed_id" do
     @relationsip.followed_id = nil
-    assert @relationsip.valid?
+    assert @relationship.valid?
   end
 end
