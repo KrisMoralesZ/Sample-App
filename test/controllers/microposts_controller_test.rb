24fd3cde65585e8e2dc2ea69d4f4da9c1be105ae
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-22 10:30:32 -0700

    fix micropost test

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
-    @micropost = microposts (:orange)
+    @micropost = microposts(:orange)
   end
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
-      post micropost_path, params: {micropost: { content: "Lorem ipsum" } }
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
     end
     assert_redirected_to login_url
   end
