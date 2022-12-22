8ad169fa03ba85b5b945ef4f08c910560fbf5641
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-22 10:24:29 -0700

    fix test

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,21 +1,22 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
+
   def setup
     @relationship = Relationship.new(follower_id: users(:user).id, followed_id: users(:other_user).id)
   end
 
-  test "sould be valid" do
+  test "should be valid" do
     assert @relationship.valid?
   end
 
   test "should require a follower_id" do
-    @relationsip.follower_id = nil
+    @relationship.follower_id = nil
     assert_not @relationship.valid?
   end
 
   test "should require a followed_id" do
-    @relationsip.followed_id = nil
-    assert @relationship.valid?
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
   end
 end
